a3a0fdd518eed455b9474f2574589c0c89e15674
Android Artisan <romartisan2025alt@gmail.com>
2025-07-20 19:37:10 +0400

    Target: zeroltexx: init

--- target/zeroltexx/config.sh
+++ target/zeroltexx/config.sh
@@ -1,1 +1,61 @@
-coming soon
+#
+# Copyright (C) 2024 BlackMesa123
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+# Device configuration file for Galaxy S6 Edge (Exynos) ()
+TARGET_NAME="Galaxy S6 Edge (Exynos)"
+TARGET_CODENAME="zeroltexx"
+TARGET_ASSERT_MODEL=("SM-G925F")
+TARGET_PLATFORM="exynos7420"
+TARGET_FIRMWARE="SM-G925F/XEF/359522060648607"
+TARGET_EXTRA_FIRMWARES=("")
+TARGET_API_LEVEL=tbf
+TARGET_PRODUCT_FIRST_API_LEVEL=tbf
+TARGET_VNDK_VERSION=tbf
+TARGET_SINGLE_SYSTEM_IMAGE="essi"
+TARGET_OS_FILE_SYSTEM="tbf"
+TARGET_SUPER_PARTITION_SIZE=tbf
+TARGET_SUPER_GROUP_NAME="group_basic"
+TARGET_SUPER_GROUP_SIZE=tbf
+TARGET_HAS_SYSTEM_EXT=tbf
+TARGET_INSTALL_METHOD=odin
+TARGET_BOOT_DEVICE_PATH="tbf"
+
+# SEC Product Feature
+TARGET_AUTO_BRIGHTNESS_TYPE="tbf"
+TARGET_DVFS_CONFIG_NAME="dvfs_policy_exynos7420_xx"
+TARGET_NFC_CHIP_VENDOR="tbf"
+TARGET_FP_SENSOR_CONFIG="tbf"
+TARGET_HAS_MASS_CAMERA_APP=tbf
+TARGET_HAS_QHD_DISPLAY=tbf
+TARGET_HFR_MODE="tbf"
+TARGET_HFR_SUPPORTED_REFRESH_RATE="tbf"
+TARGET_HFR_DEFAULT_REFRESH_RATE="tbf"
+TARGET_DISPLAY_CUTOUT_TYPE="none"
+TARGET_HFR_SEAMLESS_BRT="none"
+TARGET_HFR_SEAMLESS_LUX="none"
+TARGET_IS_ESIM_SUPPORTED=tbf
+TARGET_HAS_HW_MDNIE=tbf
+TARGET_MDNIE_SUPPORTED_MODES="tbf"
+TARGET_MDNIE_WEAKNESS_SOLUTION_FUNCTION="tbf"
+TARGET_SUPPORT_WIFI_7=false
+TARGET_SUPPORT_HOTSPOT_DUALAP=false
+TARGET_SUPPORT_HOTSPOT_WPA3=false
+TARGET_SUPPORT_HOTSPOT_6GHZ=false
+TARGET_SUPPORT_HOTSPOT_WIFI_6=false
+TARGET_SUPPORT_HOTSPOT_ENHANCED_OPEN=false
+TARGET_AUDIO_SUPPORT_ACH_RINGTONE=false
+TARGET_AUDIO_SUPPORT_VIRTUAL_VIBRATION=false
