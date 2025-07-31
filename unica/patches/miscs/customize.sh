e73f26ca2083e48b4f160417903511286be72118
Android-Artisan <romartisan2025@gmail.com>
2025-07-31 15:54:46 +0400

    unica: patches: misc: change display id to Ultra Legacy

--- unica/patches/miscs/customize.sh
+++ unica/patches/miscs/customize.sh
@@ -3,7 +3,7 @@ REGION=$(echo -n "$TARGET_FIRMWARE" | cut -d "/" -f 2)
 
 # Set build ID
 VALUE="$(GET_PROP "$WORK_DIR/system/system/build.prop" "ro.build.display.id")"
-SET_PROP "system" "ro.build.display.id" "ArtisanROM $ROM_CODENAME $ROM_VERSION - $TARGET_CODENAME"
+SET_PROP "system" "ro.build.display.id" "ArtisanROM Ultra Legacy $ROM_VERSION - $TARGET_CODENAME (ExtremeROM)"
 
 # Disable FRP
 SET_PROP "vendor" "ro.frp.pst" ""
