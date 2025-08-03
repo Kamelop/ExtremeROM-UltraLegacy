7d76df6f5744e35d4f3df313c06126ba9afd673d
Android-Artisan <romartisan2025@gmail.com>
2025-08-03 20:09:05 +0400

    Update artisan.sh to add new WIP devices

--- artisan.sh
+++ artisan.sh
@@ -69,6 +69,9 @@ echo -e "  ${CYAN}crownlte${NC}   → Samsung Galaxy Note9"
 echo -e "  ${CYAN}r7n${NC}        → Samsung Galaxy Note10 Lite"
 echo -e "  ${CYAN}star2lte${NC}   → Samsung Galaxy S9+"
 echo -e "  ${CYAN}starlte${NC}    → Samsung Galaxy S9"
+echo -e "  ${CYAN}e1s${NC}       → Samsung Galaxy S24 (WIP)"
+echo -e "  ${CYAN}e2s${NC}       → Samsung Galaxy S24+ (WIP)"
+echo -e "  ${CYAN}r12s${NC}       → Samsung Galaxy S24FE (WIP)"
 echo
 
 read -p "Enter codename (e.g., crownlte): " codename
